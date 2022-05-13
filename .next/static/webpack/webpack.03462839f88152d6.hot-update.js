b931d3228d5acfdd4c839400687884e820646081
vincentdchn <vincent@MacBook-Pro-Vincent.local>
2022-05-13 22:50:53 +0200

    changing title

--- .next/static/webpack/webpack.03462839f88152d6.hot-update.js
+++ .next/static/webpack/webpack.03462839f88152d6.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1a1814245e7b6418"; }
+/******/ 	__webpack_require__.h = function() { return "a164f36a0dcb0714"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
